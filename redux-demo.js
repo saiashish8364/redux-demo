6490571d6f834fe4a06fb4381d7f20c9c022b1d6
saiashish8364 <polusaiashish97@gmail.com>
2023-07-14 12:52:09 +0530

    2

--- redux-demo.js
+++ redux-demo.js
@@ -1,8 +1,15 @@
 const redux = require("redux");
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === "dispatch") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
 };
 
 const store = redux.createStore(counterReducer);
@@ -14,3 +21,9 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: "dispatch" });
+store.dispatch({ type: "dispatch" });
+store.dispatch({ type: "dispatch" });
+store.dispatch({ type: "dispatch" });
+store.dispatch({ type: "dispatch" });
+
+store.dispatch({ type: "decrement" });
